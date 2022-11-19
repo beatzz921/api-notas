88c90163ec2b631b39f294d1e308614d0d799da7
Jendry <jendryuv@gmail.com>
2022-11-19 01:43:54 -0500

    commit actualizacion 6

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -76,6 +76,65 @@ class ApiController extends AbstractFOSRestController
         $em->persist($nota);
         $em->flush();
 
-        return new JsonResponse('Se ha creado la nota satisfactoriamente', 201);
+        return new JsonResponse(['message' => 'Se ha creado la nota satisfactoriamente', 'code' => 201, 'status' => 'success']);
+    }
+
+    /**
+     * @Rest\Post("/nota/editar", name="nota_editar")
+     */
+    public function editarAction(Request $request, NotaRepository $notaRepository, TagRepository $tagRepository): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $id  = $request->get('id');
+        $nota = $notaRepository->findOneById($id);
+
+        if (Is_null($nota)) {
+            return new JsonResponse(['message' => 'No se ha encontrado la nota', 'code' => 404, 'satus' => 'error']);
+        } else {
+            if (
+                is_null($request->get('titulo')) or
+                is_null($request->get('descripcion')) or
+                is_null($request->get('tags')) or
+                is_null($request->get('publica'))
+            ) {
+                return new JsonResponse(['message' => 'Faltan parámetros en la petición', 'code' => 400, 'satus' => 'error']);
+            } else {
+                $titulo  = $request->get('titulo');
+                $descripcion  = $request->get('descripcion');
+                $tituloTags  = $request->get('tags');
+
+                if ($request->get('publica') == 1) {
+                    $publica  = true;
+                } else {
+                    $publica  = false;
+                }
+
+                foreach ($tituloTags as $tituloTag) {
+                    $tag = $tagRepository->findOneByTitulo($tituloTag);
+                    if (is_null($tag)) {
+                        // Si el tag no existe lo creamos y 
+                        //luego lo añadimos al array de tags de la nueva nota
+                        $tag = new Tag();
+                        $tag->setTitulo($tituloTag);
+                        $em->persist($tag);
+
+                        $nota->addTag($tag);
+                    } else {
+                        // Si el tag ya existe lo añadimos al array de tags de la nueva nota
+                        $nota->addTag($tag);
+                    }
+                }
+
+                $nota->setTitulo($titulo);
+                $nota->setDescripcion($descripcion);
+                $nota->setPublica($publica);
+
+                $em->persist($nota);
+                $em->flush();
+
+                return new JsonResponse(['message' => 'Se ha editado la nota satisfactoriamente', 'code' => 201, 'satus' => 'success']);
+            }
+        }
     }
 }
