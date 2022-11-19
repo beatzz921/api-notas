640c6ddb858a2a0572374c07959e785ea69234a5
Jendry <jendryuv@gmail.com>
2022-11-19 01:49:49 -0500

    commit actualizacion 7

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -36,47 +36,57 @@ class ApiController extends AbstractFOSRestController
     {
         $em = $this->getDoctrine()->getManager();
         $nota = new Nota();
-        $titulo  = $request->get('titulo');
-        $descripcion  = $request->get('descripcion');
-        $tituloTags  = $request->get('tags');
-        $usuarioEmail  = $request->get('usuario');
-
-        if ($request->get('publica') == 1) {
-            $publica  = true;
+        if (
+            is_null($request->get('titulo')) or
+            is_null($request->get('descripcion')) or
+            is_null($request->get('tags')) or
+            is_null($request->get('publica') or
+            is_null($request->get('usuario')))
+        ) {
+            return new JsonResponse(['message' => 'Faltan parámetros en la petición', 'code' => 400, 'status' => 'error']);
         } else {
-            $publica  = false;
-        }
+            $titulo  = $request->get('titulo');
+            $descripcion  = $request->get('descripcion');
+            $tituloTags  = $request->get('tags');
+            $usuarioEmail  = $request->get('usuario');
 
-        foreach ($tituloTags as $tituloTag) {
-            $tag = $tagRepository->findOneByTitulo($tituloTag);
-            if (is_null($tag)) {
-                // Si el tag no existe lo creamos y 
-                //luego lo añadimos al array de tags de la nueva nota
-                $tag = new Tag();
-                $tag->setTitulo($tituloTag);
-                $em->persist($tag);
-
-                $nota->addTag($tag);
+            if ($request->get('publica') == 1) {
+                $publica  = true;
             } else {
-                // Si el tag ya existe lo añadimos al array de tags de la nueva nota
-                $nota->addTag($tag);
+                $publica  = false;
             }
+
+            foreach ($tituloTags as $tituloTag) {
+                $tag = $tagRepository->findOneByTitulo($tituloTag);
+                if (is_null($tag)) {
+                    // Si el tag no existe lo creamos y 
+                    //luego lo añadimos al array de tags de la nueva nota
+                    $tag = new Tag();
+                    $tag->setTitulo($tituloTag);
+                    $em->persist($tag);
+
+                    $nota->addTag($tag);
+                } else {
+                    // Si el tag ya existe lo añadimos al array de tags de la nueva nota
+                    $nota->addTag($tag);
+                }
+            }
+
+            // ASUMO Q EL EMAIL DEL USUARIO Q VIENE EN LA PETICION
+            // YA SE ENCUENTRA INSERTADO EN LA BD
+            $usuario = $userRepository->findOneByEmail($usuarioEmail);
+            $nota->setUsuario($usuario);
+            $nota->setTitulo($titulo);
+            $nota->setDescripcion($descripcion);
+            $nota->setPublica($publica);
+            $nota->setEliminada(false);
+            $nota->setFechaEliminada(null);
+
+            $em->persist($nota);
+            $em->flush();
+
+            return new JsonResponse(['message' => 'Se ha creado la nota satisfactoriamente', 'code' => 201, 'status' => 'success']);
         }
-
-        // ASUMO Q EL EMAIL DEL USUARIO Q VIENE EN LA PETICION
-        // YA SE ENCUENTRA INSERTADO EN LA BD
-        $usuario = $userRepository->findOneByEmail($usuarioEmail);
-        $nota->setUsuario($usuario);
-        $nota->setTitulo($titulo);
-        $nota->setDescripcion($descripcion);
-        $nota->setPublica($publica);
-        $nota->setEliminada(false);
-        $nota->setFechaEliminada(null);
-
-        $em->persist($nota);
-        $em->flush();
-
-        return new JsonResponse(['message' => 'Se ha creado la nota satisfactoriamente', 'code' => 201, 'status' => 'success']);
     }
 
     /**
